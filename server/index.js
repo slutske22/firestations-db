5e9baf570555af1cf6e36500b4b60348185ef735
Seth Lutske <seth.lutske@gmail.com>
2020-06-09 21:00:05 +0000

    successfuly connecting to MongoDB Atlas Cluster

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
 
@@ -6,4 +7,15 @@ const PORT = 3000
 
 app.use(express.static('../client/dist'))
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+const uri = "mongodb+srv://slutske22:FSCluster@fscluster-tmsah.mongodb.net/FSCluster?retryWrites=true&w=majority";
+mongoose.connect(uri, {
+   useNewUrlParser: true,
+   useUnifiedTopology: true
+});
+const db = mongoose.connection
+db.once('open', function() {
+  console.log('Connected to FSCluster DB Successfully')
+});
+
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`)) 
