17dee21a818c10533b0170093dbf13631c7e86d8
slutske22 <seth.lutske@gmail.com>
2020-06-08 22:30:49 -0700

    adding all the setup

---
+++ client/webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const HOST = process.env.HOST || 'localhost';
+const PORT = process.env.PORT || 8080;
+const PROXY = `http://${HOST}:${PORT}`;
+
+module.exports = {
+   entry: './src/index.js',
+   output: {
+      publicPath: '/'
+   },
+   module: {
+      rules: [
+         {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: ['babel-loader']
+         },
+         {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+         },
+         {
+            test: /\.scss$/,
+            use: ['style-loader', 'css-loader', 'sass-loader']
+         },
+         {
+            test: /\.(png|jpg|gif)$/,
+            use: ['file-loader']
+         },
+         {
+            test: /\.html$/,
+            use: ['html-loader']
+         }
+      ]
+   },
+   devServer: {
+      historyApiFallback: true
+   },
+   plugins: [
+      new HtmlWebpackPlugin({
+         template: './src/index.html',
+         filename: './index.html'
+      })
+   ]
+}
