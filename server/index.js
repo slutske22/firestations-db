d3b6a0ab8ee8628d68028a594655f5f188eeab53
Seth Lutske <52637023+slutske22@users.noreply.github.com>
2020-06-09 21:02:43 -0700

    successfully pulling data from MongoDB Atlas FireStarter DB FEMA_Stations collection

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const Schema = require('./StationSchema')
 
 const app = express()
 
@@ -7,15 +8,40 @@ const PORT = 3000
 
 app.use(express.static('../client/dist'))
 
-const uri = "mongodb+srv://slutske22:FSCluster@fscluster-tmsah.mongodb.net/FSCluster?retryWrites=true&w=majority";
+
+
+
+
+
+const uri = "mongodb+srv://slutske22:FSCluster@fscluster-tmsah.mongodb.net/FireStarter?retryWrites=true&w=majority";
+
 mongoose.connect(uri, {
    useNewUrlParser: true,
    useUnifiedTopology: true
 });
+
 const db = mongoose.connection
+
 db.once('open', function() {
-  console.log('Connected to FSCluster DB Successfully')
+  console.log('Connected to FireStarter DB Successfully')
+
+  find("FEMA_stations", {"HQ city": "San Diego"}, (err, docs) => {
+    console.dir(docs)
+  })
+
 });
+
+
+
+function find (name, query, cb) {
+    mongoose.connection.db.collection(name, function (err, collection) {
+       collection.find(query).toArray(cb);
+   });
+}
+
+
+
+
 
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`)) 
