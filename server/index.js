06062827e2295e3cbd278a660544f7ea6fca663e
Seth Lutske <seth.lutske@gmail.com>
2020-07-13 19:06:50 +0000

    changing PORT for heroku

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ import cors from 'cors'
 import routes from './routes'
 
 const app = express()
-const PORT = 3000
+const PORT = process.env.port || 3000
 
 app.use(express.static('../client/dist'))
 
