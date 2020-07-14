91c8a1ee63e8a662f486e2e3de91240fdeb33284
slutske22 <seth.lutske@gmail.com>
2020-07-13 18:44:34 -0700

    port....seriously?

--- server/src/index.js
+++ server/src/index.js
@@ -6,7 +6,7 @@ import path from 'path'
 import routes from './routes'
 
 const app = express()
-const PORT = process.env.port || 3000
+const PORT = process.env.PORT || 3000
 
 app.use(express.static(path.resolve(__dirname, '../../client/dist')))
 console.log(__dirname)
