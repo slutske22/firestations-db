3208db5a4655f8c95c25f9fd44a97dcec2b83ed4
Seth Lutske <seth.lutske@gmail.com>
2021-02-04 21:24:20 -0800

    Fixing broken mapbox tile link, switching to thunderforest

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,72 +1,63 @@
-import React, { useRef, useState, useEffect } from 'react'
-import { Map as LeafletMap, TileLayer, Marker } from 'react-leaflet'
-import { useSelector, useDispatch } from 'react-redux'
+import React, { useRef, useState, useEffect } from 'react';
+import { Map as LeafletMap, TileLayer, Marker } from 'react-leaflet';
+import { useSelector, useDispatch } from 'react-redux';
 
-import { setMapReference, setMapZoom, setMapBounds, getStations as callForStations } from '../store/actions/mapActions'
-import { initialState } from '../store/reducers/mapReducers'
+import {
+	setMapReference,
+	setMapZoom,
+	setMapBounds,
+	getStations as callForStations,
+} from '../store/actions/mapActions';
+import { initialState } from '../store/reducers/mapReducers';
 
-import GeoSearch from './GeoSearch'
-import FireStations from './FireStations'
-
-const mapboxAccessToken = 'pk.eyJ1Ijoic2x1dHNrZTIyIiwiYSI6ImNqeGw1Y3BibDAybG4zeHFyaXl3OXVwZXUifQ.fZ_5Raq5z-DUpo2AK-bQHA'
+import GeoSearch from './GeoSearch';
+import FireStations from './FireStations';
 
 const Map = () => {
+	const zoomThreshhold = useSelector((state) => state.map.zoomThreshhold);
+	const currentZoom = useSelector((state) => state.map.zoom);
 
-   const zoomThreshhold = useSelector(state => state.map.zoomThreshhold)
-   const currentZoom = useSelector(state => state.map.zoom)
+	const mapRef = useRef(null);
+	const { zoom, center } = initialState;
+	const dispatch = useDispatch();
 
-   const mapRef = useRef(null)
-   const { zoom, center } = initialState
-   const dispatch = useDispatch()
+	useEffect(() => {
+		window.map = mapRef.current.leafletElement;
+		dispatch(setMapReference(mapRef.current.leafletElement));
+		getStations();
+	}, []);
 
-   useEffect(() => {
-      window.map = mapRef.current.leafletElement
-      dispatch( setMapReference(mapRef.current.leafletElement) )
-      getStations()
-   }, [])
+	const getStations = () => {
+		const map = mapRef.current.leafletElement;
+		const zoom = map.getZoom();
+		const bounds = map.getBounds();
+		dispatch(setMapZoom(zoom));
+		dispatch(setMapBounds(bounds));
 
-   const getStations = () => {
-      
-      const map = mapRef.current.leafletElement
-      const zoom = map.getZoom()
-      const bounds = map.getBounds()
-      dispatch( setMapZoom(zoom) )
-      dispatch( setMapBounds(bounds) )
+		if (zoom >= zoomThreshhold) {
+			callForStations({ bounds });
+		}
+	};
 
-      if (zoom >= zoomThreshhold){
-         callForStations({ bounds })
-      }
+	return (
+		<LeafletMap
+			center={center}
+			zoom={zoom}
+			id="mapID"
+			ref={mapRef}
+			onMoveEnd={getStations}
+		>
+			<GeoSearch
+				position="topleft"
+				useMapBounds={false}
+				providers={['arcgisOnlineProvider', 'geocodeServiceProvider']}
+			/>
 
-   }
+			<TileLayer url="https://tile.thunderforest.com/atlas/{z}/{x}/{y}.png?apikey=e19950ff5cbf4a589528ddb5134c193e" />
 
+			{currentZoom >= zoomThreshhold && <FireStations />}
+		</LeafletMap>
+	);
+};
 
-   return (
-
-      <LeafletMap 
-         center={center} 
-         zoom={zoom}
-         id="mapID"
-         ref={mapRef}
-         onMoveEnd={getStations} >
-
-         <GeoSearch 
-            position="topleft"
-            useMapBounds={false}
-            providers={['arcgisOnlineProvider', 'geocodeServiceProvider']} />
-
-         {/* Support depreciated for mapbox classic styles, but they still work: */}
-         <TileLayer
-            url={`https://api.tiles.mapbox.com/v4/mapbox.outdoors/{z}/{x}/{y}.png?access_token=${mapboxAccessToken}`}
-            attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
-            id="mapbox.outdoors" />
-
-            {currentZoom >= zoomThreshhold && <FireStations />}
-
-
-      </LeafletMap>
-
-   )
-
-}
-
-export default Map
+export default Map;
